37c0cf577daa55384529a9ebb9570856acebd35a
Bartosz Grochal <radu480@o2.pl>
2019-07-28 12:09:30 +0200

    Created Account service.

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  readonly controllerURL = 'http://localhost:54277/api/Account';
+
+  constructor(private http: HttpClient) { }
+
+  login(formData){
+    
+  }
+
+}
