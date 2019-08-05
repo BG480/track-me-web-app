466f74daecc7f81c17e90169775eda9b9443f613
Bartosz Grochal <radu480@o2.pl>
2019-08-05 22:19:29 +0200

    Updated routing module for user page component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
-import {RegistrationComponent} from './components/registration/registration.component';
-import {UserPageComponent} from './components/user-page/user-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { UserHomeComponent } from './components/user-page/user-home/user-home.component';
+import { UserAccountComponent } from './components/user-page/user-account/user-account.component';
+import { UserTripsComponent } from './components/user-page/user-trips/user-trips.component';
+import { UserTripDetailsComponent } from './components/user-page/user-trip-details/user-trip-details.component';
+
 import { AuthGuardService } from './services/auth-guard.service';
 
 
@@ -14,7 +19,25 @@ const routes: Routes = [
     canActivate: [AuthGuardService],
     data: {
       expectedRole: 'BasicUser'
-  }
+    },
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+      },
+      { 
+        path: 'home', 
+        component: UserHomeComponent
+      },
+      { path: 'account', 
+        component: UserAccountComponent
+      },
+      { 
+        path: 'trips', 
+        component: UserTripsComponent
+      }
+    ],
   }
 ];
 
