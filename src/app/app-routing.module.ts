750eeeddc8602598d05fa578a275621178ce3290
Bartosz Grochal <radu480@o2.pl>
2019-08-12 23:15:17 +0200

    Fixed routing errors.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './containers/login-page/login-page.component';
 import { RegistrationPageComponent } from './containers/registration-page/registration-page.component';
-import { UserPageComponent } from './components/user-page/user-page.component';
-import { UserHomeComponent } from './components/user-page/user-home/user-home.component';
-import { UserAccountComponent } from './components/user-page/user-account/user-account.component';
-import { UserTripsComponent } from './components/user-page/user-trips/user-trips.component';
-import { UserTripDetailsComponent } from './components/user-page/user-trip-details/user-trip-details.component';
-
 import { AuthGuardService } from './services/auth-guard.service';
 import { BasicUserPageComponent } from './containers/basic-user-page/basic-user-page.component';
 import { BasicUserHomeComponent } from './components/basic-user/basic-user-home/basic-user-home.component';
