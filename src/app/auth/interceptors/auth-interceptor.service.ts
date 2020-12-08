e9d7f5995234b6f2efbf499d4c86d26590dfbbbb
Bartosz <radu480@o2.pl>
2020-12-08 23:25:58 +0100

    Modified auth interceptor.

--- src/app/auth/interceptors/auth-interceptor.service.ts
+++ src/app/auth/interceptors/auth-interceptor.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { exhaustMap, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-    if (localStorage.getItem('token') != null) {
-      return next.handle(
-        req.clone({
-          headers: req.headers.append('Authorization', 'Bearer ' + localStorage.getItem('token'))
-        })
-      );
-    }
-    return next.handle(req);
+    return this.authService.loggedUser.pipe(
+      take(1),
+      exhaustMap(user => {
+        if (!user) {
+          return next.handle(req);
+        }
+        const modifiedRequest = req.clone({
+          headers: req.headers.append('Authorization', 'Bearer ' + user.token)
+        });
+        return next.handle(modifiedRequest);
+      })
+    );
   }
 }
