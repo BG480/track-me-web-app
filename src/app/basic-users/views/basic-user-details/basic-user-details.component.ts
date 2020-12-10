d0b9bfd31b5971f005f920474df3feff02987076
Bartosz <radu480@o2.pl>
2020-12-11 00:11:15 +0100

    Prepared views for basic users.

--- src/app/basic-users/views/basic-user-details/basic-user-details.component.ts
+++ src/app/basic-users/views/basic-user-details/basic-user-details.component.ts
@@ -1,4 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BasicUser } from '../../models/basic-user.model';
+import { BasicUserService } from '../../services/basic-user.service';
 
 @Component({
   selector: 'app-basic-user-details',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicUserDetailsComponent implements OnInit {
 
-  constructor() { }
+  basicUser: BasicUser;
 
-  ngOnInit(): void {
-  }
+  constructor(private route: ActivatedRoute,
+    private basicUserService: BasicUserService,
+    private location: Location) { }
+
+    ngOnInit() {
+      this.getBasicUserDetails();
+    }
+  
+    getBasicUserDetails(): void {
+      const id = this.route.snapshot.paramMap.get('id');
+      this.basicUserService.getBasicUser(id).subscribe(
+        (basicUser: BasicUser) => {
+          this.basicUser = basicUser
+        },
+        err => {
+          if (err.status == 404){
+            // this.toastr.error(err.error.message); TODO TOASTR
+          }
+          else{
+            // this.toastr.error("Error occurred."); TODO TASTR
+          }
+        });
+  
+    }
+  
+    public goBack(): void {
+      this.location.back();
+    }
 
 }
--- src/app/basic-users/views/basic-user-list/basic-user-list.component.ts
+++ src/app/basic-users/views/basic-user-list/basic-user-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BasicUser } from '../../models/basic-user.model';
+import { BasicUserService } from '../../services/basic-user.service';
 
 @Component({
   selector: 'app-basic-user-list',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicUserListComponent implements OnInit {
 
-  constructor() { }
+  basicUsers: BasicUser[];
 
-  ngOnInit(): void {
+  constructor(private basicUserService: BasicUserService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.getBasicUsers();
+  }
+
+  getBasicUsers(): void {
+    this.basicUserService.getBasicUsers()
+    .subscribe(basicUsers => this.basicUsers = basicUsers);
+  }
+
+  showBasicUserDetails(basicUser: BasicUser): void {
+    this.router.navigateByUrl('admin/basic-user-details/' + basicUser.id);
+  }
+
+  deleteBasicUser(basicUser: BasicUser): void {
+    this.basicUserService.deleteBasicUser(basicUser.id).subscribe(
+      (result: any) => {
+        // this.toastr.success("User successfully deleted."); // TODO TOASTR
+        this.getBasicUsers();
+      },
+      err => {
+        if (err.status == 404){
+          // this.toastr.error(err.error.message); // TODO TOASTR
+        }
+        else{
+          // this.toastr.error("Error occurred."); // TODO TOASTR 
+        }
+      }
+    );
   }
 
 }
