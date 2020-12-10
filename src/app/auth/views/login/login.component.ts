65d822b39eeeed2e234d742493f2c58a91807457
Bartosz <radu480@o2.pl>
2020-12-10 18:25:33 +0100

    Prepared login view template with logic.

--- src/app/auth/views/login/login.component.ts
+++ src/app/auth/views/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    Email: new FormControl('', Validators.required),
+    Password: new FormControl('', Validators.required),
+  })
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if(this.loginForm.valid) {
+      this.authService.login(this.loginForm.get('Email').value, this.loginForm.get('Password').value).subscribe(
+        (result: any) => {
+          this.router.navigateByUrl("user/home");
+        },
+        err => {
+          if (err.status == 404){
+            // this.toastr.error(err.error.message); TODO: wyświetlić powiadomienie z serwisu do powadomień
+          }
+          else{
+            // this.toastr.error("Error occurred."); TODO: wyświetlić powiadomienie z serwisu do powadomień
+          }       
+        }
+      );
+    } else {
+      // this.toastr.error("Invalid data.") TODO: wyświetlić powiadomienie z serwisu do powadomień
+    }
   }
 
 }
