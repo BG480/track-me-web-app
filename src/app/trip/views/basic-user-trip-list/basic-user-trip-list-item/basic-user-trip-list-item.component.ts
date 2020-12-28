9a2a9889a01f39b1d660c950369df046e5d3cb94
Bartosz <radu480@o2.pl>
2020-12-28 23:41:04 +0100

    Prepared basic user trip list view.

--- src/app/trip/views/basic-user-trip-list/basic-user-trip-list-item/basic-user-trip-list-item.component.ts
+++ src/app/trip/views/basic-user-trip-list/basic-user-trip-list-item/basic-user-trip-list-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Trip } from 'src/app/trip/models/trip.model';
+import { TripService } from 'src/app/trip/services/trip.service';
 
 @Component({
   selector: 'app-basic-user-trip-list-item',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-user-trip-list-item.component.css']
 })
 export class BasicUserTripListItemComponent implements OnInit {
-
-  constructor() { }
+  @Input() trip: Trip;
+  @Input() index: number;
+  
+  constructor(private tripService: TripService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onTripSensorData(): void {
+    this.router.navigateByUrl('trip/' + this.trip.tripId + '/sensor-data');
+  }
+
+  onDeleteTrip(): void {
+    this.tripService.deleteTrip(this.trip.tripId).subscribe(
+      (result: any) => {
+        //this.toastr.success("Admin successfully deleted.")
+        this.router.navigateByUrl('list');
+      },
+      (error: string) => {
+        //this.toastr.success("Admin successfully deleted.")
+      }
+    );
   }
 
 }
