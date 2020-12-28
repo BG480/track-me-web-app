395726030813863078ac0bc34d14569d5bf977da
Bartosz <radu480@o2.pl>
2020-12-28 22:00:59 +0100

    Improved service for basic user.

--- src/app/basic-user/services/basic-user.service.ts
+++ src/app/basic-user/services/basic-user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { BasicUser } from '../models/basic-user.model';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,16 +14,35 @@ export class BasicUserService {
 
   constructor(private http: HttpClient) { }
 
-  getBasicUsers(): Observable<BasicUser[]>{
-    return this.http.get<BasicUser[]>(this.controllerUrl);
+  getBasicUsers(): Observable<BasicUser[]> {
+    return this.http
+    .get<BasicUser[]>(this.controllerUrl)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
-  getBasicUser(id: string): Observable<BasicUser>{
-    return this.http.get<BasicUser>(this.controllerUrl + '/' + id);
+  getBasicUser(id: string): Observable<BasicUser> {
+    return this.http
+    .get<BasicUser>(this.controllerUrl + '/' + id)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
   deleteBasicUser(id: number): Observable<{}> {
-    return this.http.delete(this.controllerUrl + '/' + id);
+    return this.http
+    .delete(this.controllerUrl + '/' + id)
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    if(!errorResponse.error || !errorResponse.error.error) {
+      return throwError('An error occurred! Try again later of contact with the support.');
+    }
+    return throwError(errorResponse.error.error);
   }
 
 }
