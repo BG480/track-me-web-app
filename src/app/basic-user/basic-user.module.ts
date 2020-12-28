cb4a9030b70153f0a9561dbc545881dc639b5ae7
Bartosz <radu480@o2.pl>
2020-12-28 21:37:45 +0100

    Fixed basic user model.

--- src/app/basic-user/basic-user.module.ts
+++ src/app/basic-user/basic-user.module.ts
@@ -5,6 +5,7 @@ import { BasicUserRoutingModule } from './basic-user-routing.module';
 import { BasicUserComponent } from './basic-user.component';
 
 import { BasicUserDetailsComponent } from './views/basic-user-details/basic-user-details.component';
+import { BasicUserListItemComponent } from './views/basic-user-list/basic-user-list-item/basic-user-list-item.component';
 import { BasicUserListComponent } from './views/basic-user-list/basic-user-list.component';
 
 
@@ -12,7 +13,8 @@ import { BasicUserListComponent } from './views/basic-user-list/basic-user-list.
     declarations: [
       BasicUserComponent,
       BasicUserDetailsComponent,
-      BasicUserListComponent
+      BasicUserListComponent,
+      BasicUserListItemComponent
     ],
     imports: [
       RouterModule,
--- src/app/basic-user/models/basic-user.model.ts
+++ src/app/basic-user/models/basic-user.model.ts
@@ -1,6 +1,6 @@
 export class BasicUser {
     basicUserId: number;
-    firtsName: string;
+    firstName: string;
     lastName: string;
     phoneNumber: string;
     email: string;
