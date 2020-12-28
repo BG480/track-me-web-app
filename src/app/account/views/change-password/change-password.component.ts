a1d2fb6b7fff9adad9e10656a44fdd6b159e6eec
Bartosz <radu480@o2.pl>
2020-12-28 22:57:38 +0100

    Prepared change password view.

--- src/app/account/views/change-password/change-password.component.ts
+++ src/app/account/views/change-password/change-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountService } from '../../services/account-service.service';
 
 @Component({
   selector: 'app-change-password',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
+  changePasswordForm = new FormGroup({
+    OldPassword: new FormControl('', Validators.required),
+    NewPassword: new FormControl('', Validators.required),
+    ConfirmPassword: new FormControl('', Validators.required)
+  });
 
-  constructor() { }
+  constructor(private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if(this.changePasswordForm.valid) {
+      this.accountService.changePassword(this.changePasswordForm.value).subscribe(
+        (result: any) => {
+          this.router.navigateByUrl("home");
+        },
+        (error: string) => {
+          // this.toastr.error(err); TODO: wyświetlić powiadomienie z serwisu do powadomień    
+        }
+      );
+    } else {
+      this.handleInvalidForm();
+    }
+  }
+
+  private handleInvalidForm() {
+    debugger; 
+    let formErrorMessage = this.getFormErrorMessage();
+    // this.toastr.error(formErrorMessage); TODO: wyświetlić powiadomienie z serwisu do powadomień    
+  }
+
+  private getFormErrorMessage(){
+    if(this.changePasswordForm.controls['OldPassword'].errors?.required) {
+      return 'Old password is required.';
+    } else if (this.changePasswordForm.controls['NewPassword'].errors?.required) {
+      return 'New password is required.';
+    } else if (this.changePasswordForm.controls['ConfirmPassword'].errors?.required) {
+      return 'You need to confirm new password.';
+    } else {
+      return 'Invalid form data.';
+    }
   }
 
 }
