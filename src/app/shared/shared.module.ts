d89c9ca61bff64487d70b83f18e3dd0173fafc85
Bartosz <radu480@o2.pl>
2020-12-30 19:52:56 +0100

    Created loading spinner component.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoadingSpinnerComponent],
   imports: [
     CommonModule
   ]
